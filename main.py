f19ad4b1ede2afb3953f7d81ff0b744b463b3a56
Jose Penalver Bartolome <penalverbj@gmail.com>
2021-03-03 22:51:04 +0000

    Added my own website to get quotes from

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 import discord
-import os 
+import os
 import requests
 import json
 
 client = discord.Client()
 
+
 def get_fortune():
-	fortune = requests.get("https://zenquotes.io/api/random")
-	json_data = json.loads(fortune.text)
+	link = "https://fortuneapi.herokuapp.com/"
+	html = requests.get(link).text
+	l = list(html)
+	for i in range(0, len(l) - 1):
+		if (l[i] == '\\' and l[i+1] == 'n'):
+			l[i] = ' '
+			l[i+1] = ''
+	return "".join(l)
+
+def get_quote():
+	line = requests.get("https://zenquotes.io/api/random")
+	json_data = json.loads(line.text)
 	quote = json_data[0]['q'] + " -" + json_data[0]['a']
 	return quote
 
+
+
 @client.event
 async def on_ready():
-	print("We have logged in as {0.user}".format(client))
+  print("We have logged in as {0.user}".format(client))
+
 
 @client.event
 async def on_message(message):
-	if message.author == client.user:
-		return
-	
-	if message.content.startswith('+f'):
-		f = get_fortune()
-		await message.channel.send(f);	
+  if message.author == client.user:
+    return
 
-client.run(os.getenv('TOKEN'));
+  if message.content.startswith('+f'):
+  	f = get_fortune()
+  	await message.channel.send(f)
+	
+
+
+client.run(os.getenv('TOKEN'))
